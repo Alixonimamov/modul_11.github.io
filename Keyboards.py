c1a4a05f37571fa234829897e046cc1948081ece
Alixonimamov <alixonimamov331@gmail.com>
2024-05-07 16:39:49 +0500

    update

--- Keyboards.py
+++ Keyboards.py
@@ -1,7 +1,7 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
-web_app = WebAppInfo()
+web_app = WebAppInfo(url="https://alixonimamov.github.io/modul_11.github.io/")
 
-app_kb=ReplyKeyboardMarkup(keyboard=[
+app_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="Mini app", web_app=web_app)]
 ], resize_keyboard=True)
