fe159bf404778de5abb9d85e771174f10fc3ed2e
Alixonimamov <alixonimamov331@gmail.com>
2024-05-11 16:29:55 +0500

    update

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ let tg=window.Telegram.Webapp;
 tg.expand();
 
 tg.MainButton.textColor="#FFFFFF"
-tg.MainButton.textColor="#FC3005"
+tg.MainButton.textColor="#fc3005"
 
 let btn1=document.getElementById("btn1")
 
-btn1.addEventListener("Click", function (){
-    tg.MainButton.setText("btn1 bosildi")
-    tg.MainButton.show()
+let rbtn1=document.getElementById("rbtn1")
+let abtn=document.getElementById("abtn1")
+
+let rbtn2=document.getElementById("rbtn2")
+let abtn2=document.getElementById("abtn2")
+
+let rbtn3=document.getElementById("rbtn3")
+let abtn3=document.getElementById("abtn3")
+
+let rbtn4=document.getElementById("rbtn4")
+let abtn4=document.getElementById("abbtn4")
+
+let item=""
+let num_count=0
+
+number=document.getElementById("count")
+abtn.addEventListener("click", function (){
+    number.innerText=num_count +1;
+    number.style.display="block";
+})
+
+btn1.addEventListener("click", function (){
+    tg.MainButton.setText("Burger")
 })
